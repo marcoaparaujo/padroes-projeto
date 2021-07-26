eee44cca45826f2587d3341fe1c779e5d3c43746
Marco Antonio Araujo <marcoaparaujo@gmail.com>
2021-07-26 10:07:16 -0300

    padrao composite

--- src/test/java/padroesestruturais/composite/DisciplinaTest.java
+++ src/test/java/padroesestruturais/composite/DisciplinaTest.java
@@ -8,19 +8,19 @@ class DisciplinaTest {
 
     @Test
     void deveRetornarEmentaDisciplina() {
-        Unidade unidade1 = new Unidade("Padroes de Criacao");
+        Unidade unidade1 = new Unidade("Padrões de Criacao");
 
-        Unidade unidade2 = new Unidade("Padroes Estruturais");
-        Topico topico21 = new Topico("Padrao Composite", 2);
+        Unidade unidade2 = new Unidade("Padrões Estruturais");
+        Topico topico21 = new Topico("Padrão Composite", 2);
         unidade2.addConteudo(topico21);
 
-        Unidade unidade3 = new Unidade("Padroes Comportamenentais");
-        Topico topico31 = new Topico("Padrao Observer", 3);
-        Topico topico32 = new Topico("Padrao State", 4);
+        Unidade unidade3 = new Unidade("Padrões Comportamentais");
+        Topico topico31 = new Topico("Padrão Observer", 3);
+        Topico topico32 = new Topico("Padrão State", 4);
         unidade3.addConteudo(topico31);
         unidade3.addConteudo(topico32);
 
-        Unidade ementa = new Unidade("Padroes de Projeto");
+        Unidade ementa = new Unidade("Padrões de Projeto");
         ementa.addConteudo(unidade1);
         ementa.addConteudo(unidade2);
         ementa.addConteudo(unidade3);
@@ -28,13 +28,13 @@ class DisciplinaTest {
         Disciplina disciplina = new Disciplina();
         disciplina.setEmenta(ementa);
 
-        assertEquals("Unidade: Padroes de Projeto\n" +
-                "Unidade: Padroes de Criacao\n" +
-                "Unidade: Padroes Estruturais\n" +
-                "Tópico: Padrao Composite - carga horária: 2\n" +
-                "Unidade: Padroes Comportamenentais\n" +
-                "Tópico: Padrao Observer - carga horária: 3\n" +
-                "Tópico: Padrao State - carga horária: 4\n", disciplina.getEmenta());
+        assertEquals("Unidade: Padrões de Projeto\n" +
+                "Unidade: Padrões de Criacao\n" +
+                "Unidade: Padrões Estruturais\n" +
+                "Tópico: Padrão Composite - carga horária: 2\n" +
+                "Unidade: Padrões Comportamentais\n" +
+                "Tópico: Padrão Observer - carga horária: 3\n" +
+                "Tópico: Padrão State - carga horária: 4\n", disciplina.getEmenta());
     }
 
     @Test
